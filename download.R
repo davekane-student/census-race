1288ab3f2eda9595f4a068527d0b2662ff7079bb
davekane-student <dave.kane+student@gmail.com>
2026-09-09 15:47:56 +0000

    uodated

--- download.R
+++ download.R
@@ -1,27 +1,63 @@
 library(tidyverse)
 library(tidycensus)
 
-# Mutually exclusive race and ethnicity variables from the 2020 Census (PL 94-171)
-race_vars <- c(
-  total = "P2_001N",
-  hispanic = "P2_002N",
-  white = "P2_005N",
-  black = "P2_006N",
-  aian = "P2_007N",
-  asian = "P2_008N",
-  nhpi = "P2_009N",
-  other = "P2_010N",
-  two_or_more = "P2_011N"
-)
-
-# Download tract-level race and ethnicity data for Sarasota County, FL
-sarasota_race <- get_decennial(
+# 2000 Decennial Census (SF1)
+d_2000 <- get_decennial(
   geography = "tract",
-  variables = race_vars,
+  variables = c(
+    total = "P004001",
+    hispanic = "P004002",
+    white = "P004005",
+    black = "P004006",
+    asian = "P004008"
+  ),
+  year = 2000,
+  state = "FL",
+  county = "Sarasota",
+  geometry = TRUE,
+  output = "wide"
+) |>
+  mutate(year = 2000)
+
+# 2010 Decennial Census (PL 94-171)
+d_2010 <- get_decennial(
+  geography = "tract",
+  variables = c(
+    total = "P002001",
+    hispanic = "P002002",
+    white = "P002005",
+    black = "P002006",
+    asian = "P002008"
+  ),
+  year = 2010,
+  sumfile = "pl",
+  state = "FL",
+  county = "Sarasota",
+  geometry = TRUE,
+  output = "wide"
+) |>
+  mutate(year = 2010)
+
+# 2020 Decennial Census (PL 94-171)
+d_2020 <- get_decennial(
+  geography = "tract",
+  variables = c(
+    total = "P2_001N",
+    hispanic = "P2_002N",
+    white = "P2_005N",
+    black = "P2_006N",
+    asian = "P2_008N"
+  ),
   year = 2020,
   state = "FL",
-  county = "Sarasota"
-)
+  county = "Sarasota",
+  geometry = TRUE,
+  output = "wide"
+) |>
+  mutate(year = 2020)
 
-# Save the raw data to the data directory
+# Combine all three decennial censuses
+sarasota_race <- bind_rows(d_2000, d_2010, d_2020)
+
+# Save the combined historical dataset
 write_rds(sarasota_race, "data/sarasota_race.rds")
